bd7287c4ec91030f188d4d01115a92507b3c073b
Jacob Stevenson <Jacobryanstevenson@gmail.com>
2021-12-17 15:42:00 -0600

    Add files via upload

---
+++ lab1/lab_tutorial_2_1_and 2_2_Stevenson/lab_tutorial_2_1_and 2_2_Stevenson/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_tutorial_2_1_and_2_2_Stevenson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("I do nothing!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

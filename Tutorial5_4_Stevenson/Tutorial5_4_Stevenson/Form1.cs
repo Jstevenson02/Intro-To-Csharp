65343ededc3ff11423f49f106cef632b101a1fb3
Jacob Stevenson <Jacobryanstevenson@gmail.com>
2021-12-17 16:01:29 -0600

    Add files via upload

---
+++ Tutorial5_4_Stevenson/Tutorial5_4_Stevenson/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Tutorial5_4_Stevenson
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+            try
+            {
+                StreamWriter outputFile;
+                outputFile = File.CreateText("Friend.txt");
+                outputFile.WriteLine(textBox1.Text);
+                outputFile.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
